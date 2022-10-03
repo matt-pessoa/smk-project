efc242cf9b52970f7fb0c8db0311cd0dacd376d8
Matt Pessoa <54649950+matt-pessoa@users.noreply.github.com>
2022-10-03 02:20:16 -0300

    assests: remove images

--- src/app/home/slides/slides.component.ts
+++ src/app/home/slides/slides.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-slides',
   templateUrl: './slides.component.html',
-  styleUrls: ['./slides.component.sass']
+  styleUrls: ['./slides.component.sass'],
 })
 export class SlidesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
