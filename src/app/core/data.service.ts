411cdc009fedd12622bdcda20f15ddac1b9cef3a
Matt Pessoa <54649950+matt-pessoa@users.noreply.github.com>
2022-10-03 04:54:11 -0300

    feat: function to get art by search

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -6,12 +6,19 @@ import { ISlides } from '../shared/interfaces';
 @Injectable()
 export class DataService {
   slideUrl: string = '../../assets/slideImages.json';
-  baseArtUrl: string = 'https://api.smk.dk/api/v1/art?';
+  baseArtUrl: string = 'https://api.smk.dk/api/v1/art/';
+  searchUrl: string = 'search?keys=';
+
+  config: string = '&offset=0&rows=10';
   lang: string = '&lang=en';
 
   constructor(private http: HttpClient) {}
 
   getSlideImages(): Observable<ISlides[]> {
     return this.http.get<ISlides[]>(this.slideUrl);
+  }
+
+  getSearchArt(search: string): Observable<{}> {
+    return this.http.get(this.baseArtUrl + this.searchUrl + search);
   }
 }
